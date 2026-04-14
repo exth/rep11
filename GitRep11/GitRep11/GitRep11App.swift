7fe7dc41098a1f43366a8c17cc704da43ce318ed
exth <exth331@gmail.com>
2026-04-14 05:06:15 +0300

    Add Xcode project

---
+++ GitRep11/GitRep11/GitRep11App.swift
@@ -0,0 +1,17 @@
+//
+//  GitRep11App.swift
+//  GitRep11
+//
+//  Created by Вадим on 14.04.26.
+//
+
+import SwiftUI
+
+@main
+struct GitRep11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
