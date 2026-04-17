01b3859b3dbb594b6d2e4c58f52d2c6ee2d6bf7b
exth <exth331@gmail.com>
2026-04-17 04:27:39 +0300

    Add MyFolder & MyView

--- GitRep11/GitRep11/ContentView.swift
+++ GitRep11/GitRep11/ContentView.swift
@@ -8,12 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    var sum: Int {
+        return 1500
+    }
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+             
+            Text(String(sum))
+            
+            Text("wqwwq")
         }
         .padding()
     }
---
+++ GitRep11/GitRep11/My Folder/MyView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct MyView: View {
+    var body: some View {
+        Text("This is my View")
+    }
+}
+
+#Preview {
+    MyView()
+}
